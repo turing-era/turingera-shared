a4ec0ef71f8240081e4ba2480fdef3baf9e7485c
bennysong <bennysong@tencent.com>
2023-05-29 22:54:24 +0800

    feat: update

--- ghttp/request.go
+++ ghttp/request.go
@@ -12,6 +12,31 @@ import (
 	"github.com/turing-era/turingera-shared/log"
 )
 
+// Get 发送Get请求
+func Get(path string, rsp interface{}, header map[string]string) error {
+	httpReq, err := http.NewRequest("GET", path, nil)
+	if err != nil {
+		return err
+	}
+	httpReq.Header.Set("Content-Type", "application/json")
+	if header != nil {
+		for k, v := range header {
+			httpReq.Header.Set(k, v)
+		}
+	}
+	resp, err := http.DefaultClient.Do(httpReq)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	log.Debugf("http rsp: %s", body)
+	if err = jsoniter.Unmarshal(body, rsp); err != nil {
+		return err
+	}
+	return nil
+}
+
 // Post 发送post请求
 func Post(path string, req interface{}, rsp interface{}, header map[string]string) error {
 	mJson, err := jsoniter.Marshal(req)
