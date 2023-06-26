527d93223c89789e6cbb853f55e1962f69545718
bennysong <bennysong@tencent.com>
2023-06-26 11:21:24 +0800

    feat: recover

--- log/server.go
+++ log/server.go
@@ -14,24 +14,24 @@ const panicBufLen = 1024
 // ServerLogInterceptor 拦截器方法
 func ServerLogInterceptor(ctx context.Context, req interface{},
 	info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	defer func() {
-		if e := recover(); e != nil {
-			buf := make([]byte, panicBufLen)
-			buf = buf[:runtime.Stack(buf, false)]
-			Errorf("[PANIC]%v\n%s\n", e, buf)
-		}
-	}()
-	reqJson := cutils.Obj2Json(req)
-	Debugf("[%v]req: %v", info.FullMethod, reqJson)
 	start := time.Now()
+	defer ServerRecover()
+	Debugf("[%v]req: %v", info.FullMethod, cutils.Obj2Json(req))
 
 	rsp, err := handler(ctx, req)
 	cost := time.Since(start).Milliseconds()
 	if err != nil {
 		Errorf("[%v][cost: %vms]err: %v", info.FullMethod, cost, err)
 	} else {
-		rspJson := cutils.Obj2Json(rsp)
-		Debugf("[%v][cost: %vms]rsp: %v", info.FullMethod, cost, rspJson)
+		Debugf("[%v][cost: %vms]rsp: %v", info.FullMethod, cost, cutils.Obj2Json(rsp))
 	}
 	return rsp, err
+}
+
+func ServerRecover() {
+	if e := recover(); e != nil {
+		buf := make([]byte, panicBufLen)
+		buf = buf[:runtime.Stack(buf, false)]
+		Errorf("[PANIC]%v\n%s\n", e, buf)
+	}
 }
